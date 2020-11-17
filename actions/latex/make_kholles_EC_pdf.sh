2461dca2c5d6c221e87f06a47c2d04119affd6ba
Olivier REYNET <olivier.reynet@ensta-bretagne.fr>
2020-11-17 12:34:35 +0100

    actions

--- actions/latex/make_kholles_EC_pdf.sh
+++ actions/latex/make_kholles_EC_pdf.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Creating output directory $OUT_DIR..."
-mkdir --parent $OUT_DIR
+[ ! -e $OUT_DIR ] &&  mkdir --parent $OUT_DIR
 
 echo "Compiling with pdflatex !"
 cd EC/
@@ -12,9 +12,10 @@ fileName="khec.tex"
 echo "Substituting version number ${GITHUB_SHA::7} in file $fileName..."
 sed -i -e "s/xxxxxxV/${GITHUB_SHA::7}/" $fileName
 
-pdflatex khec.tex
+pdflatex $fileName 
+pdflatex $fileName
 cd ..
 
 echo "Copying result to PDF/"
-cp EC/khec.pdf $OUT_DIR
+cp -f EC/khec.pdf $OUT_DIR
 
