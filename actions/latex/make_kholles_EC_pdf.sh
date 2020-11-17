f5acd852a4d3e3492db1cc662f0df1a77fea5418
Olivier Reynet <olivier.reynet@ensta-bretagne.fr>
2020-11-17 17:52:20 +0100

    Update make_kholles_EC_pdf.sh

--- actions/latex/make_kholles_EC_pdf.sh
+++ actions/latex/make_kholles_EC_pdf.sh
@@ -12,14 +12,18 @@ fileName="khec.tex"
 main="main.tex"
 d=`date +%d-%m-%Y` 
 echo "Substituting version number ${GITHUB_SHA::7} in file $fileName..."
-sed  -e "/-- Document version :/c -- Document version : ${GITHUB_SHA::7} -- le  $d/" $fileName > $main
+sed  -e "/-- Document version :/c -- Document version : ${GITHUB_SHA::7} -- le  $d" $fileName > $main
 
 pdflatex $main 
 pdflatex $main
-rm -f $main
-cd ..
 
 echo "Copying result to PDF/"
 outFile="main.pdf"
-cp -f EC/$outFile $OUT_DIR/colles_EC.pdf
+cp -f $outFile ../$OUT_DIR/colles_EC.pdf
+
+echo "Removing temp files..."
+rm -f main*
+cd ..
+
+
 
